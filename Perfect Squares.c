bef066454eaf300cb256c0a0ae81c9ad2d0747d2
AtharvaAnbhule <125785886+AtharvaAnbhule@users.noreply.github.com>
2024-02-08 13:33:14 +0530

    Create Perfect Squares.c
    Given an integer n, return the least number of perfect square numbers that sum to n.
    
    A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
    
     
    
    Example 1:
    
    Input: n = 12
    Output: 3
    Explanation: 12 = 4 + 4 + 4.
    Example 2:
    
    Input: n = 13
    Output: 2
    Explanation: 13 = 4 + 9.
     
    
    Constraints:
    
    1 <= n <= 104

---
+++ Perfect Squares.c
@@ -0,0 +1,13 @@
+int min(int a, int b){
+    return (a<b) ? a:b;
+}
+int numSquares(int n) {
+     int dp[n+1];
+    for(int i=0; i<=n; i++) dp[i] = i; // put in dp array
+    for(int i=1; i<=n; i++){ 
+        for(int j=1; j*j<=i; j++){//for square numbers
+            dp[i] = min(dp[i], 1+dp[i-j*j]); 
+        }
+    }
+    return dp[n];
+}
